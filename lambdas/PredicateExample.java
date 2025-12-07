102070cca82480274dfb303ee3480af2bfd365cd
CarlosDevCodex <cjava.201901@gmail.com>
2025-12-07 14:31:18 -0500

    7.12.2025 14.30  se agregó la clase predicate

---
+++ lambdas/PredicateExample.java
@@ -0,0 +1,17 @@
+import java.util.function.Predicate;
+
+public class PredicateExample {
+
+  public static void main(String[] args) {
+    /*
+       Recibe un valor  y devuelve  un booleano (true o false)
+    */
+
+       Predicate<String>  predicate=(a)->{
+             return a.equals("a");
+         };
+          System.out.println(predicate.test("a"));  
+
+  } 
+
+}
